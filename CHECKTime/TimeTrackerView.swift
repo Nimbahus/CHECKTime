dda43a2adf61dd6866b75f05348eb535d0094232
maurizio.volanti <maurizio.volanti@check24.de>
2023-05-12 15:58:19 +0200

    added TimeTrackerView

--- CHECKTime/TimeTrackerView.swift
+++ CHECKTime/TimeTrackerView.swift
@@ -8,62 +8,86 @@
 import SwiftUI
 import CoreData
 
-struct CircularProgressView: View {
-    let progress: Double
-    
-    var body: some View {
-        ZStack {
-            Circle()
-                .stroke(
-                    Color.pink.opacity(0.5),
-                    lineWidth: 30
-                )
-            Circle()
-                .trim(from: 0, to: progress)
-                .stroke(
-                    Color.pink,
-                    style: StrokeStyle(
-                        lineWidth: 30,
-                        lineCap: .round
-                    )
-                )
-                .rotationEffect(.degrees(-90))
-                .animation(.easeOut, value: progress)
+protocol TimeTrackerViewDelegate: AnyObject {
+    func didStartTimer()
+    func didPauseTimer()
+}
 
-        }
+class TimeTrackerViewModel: ObservableObject, Identifiable {
+    @Published var progress: Double
+    @Published var currentTimer: String
+    @Published var delegate: TimeTrackerViewDelegate?
+
+    init(progress: Double, currentTimer: String, delegate: TimeTrackerViewDelegate? = nil) {
+        self.progress = progress
+        self.currentTimer = currentTimer
+        self.delegate = delegate
+    }
+    
+    func updateTimer(progress: Double, currentTimer: String) {
+        self.progress = progress
+        self.currentTimer = currentTimer
     }
 }
 
 struct TimeTrackerView: View {
-    @State var progress: Double = 0
-    
+    @ObservedObject var model: TimeTrackerViewModel
+
+    @State var trackerButtonImageSource: String = "play.fill"
+    @State var trackerIsActive: Bool = false
+        
     var body: some View {
         VStack {
+            Text(model.currentTimer)
+                .font(.largeTitle)
+                .bold()
             Spacer()
             ZStack {
-                CircularProgressView(progress: progress)
-                Text("\(progress * 100, specifier: "%.0f")")
-                    .font(.largeTitle)
-                    .bold()
+                ZStack {
+                    Circle()
+                        .stroke(
+                            Color.pink.opacity(0.5),
+                            lineWidth: 30
+                        )
+                    Circle()
+                        .trim(from: 0, to: CGFloat(model.progress))
+                        .stroke(
+                            Color.pink,
+                            style: StrokeStyle(
+                                lineWidth: 30,
+                                lineCap: .round
+                            )
+                        )
+                        .rotationEffect(.degrees(-90))
+                        .animation(.easeOut, value: model.progress)
+
+                }
+                Button {
+                    trackerIsActive.toggle()
+                    if trackerIsActive {
+                        model.delegate?.didStartTimer()
+                    } else {
+                        model.delegate?.didPauseTimer()
+                    }
+                    trackerButtonImageSource = trackerIsActive ? "pause.fill" : "play.fill"
+                    
+                } label: {
+                    Image(systemName: trackerButtonImageSource)
+                        .resizable()
+                        .frame(width: 32.0, height: 32.0)
+                }
+                .buttonStyle(PlainButtonStyle())
+                .controlSize(.large)
             }.frame(width: 200, height: 200)
             Spacer()
-            HStack {
-                Slider(value: $progress, in: 0...1)
-                Button("Reset") {
-                    resetProgress()
-                }.buttonStyle(.borderedProminent)
-            }
         }
-    }
-    
-    func resetProgress() {
-        progress = 0
     }
 }
 
 
 struct TimeTrackerView_Previews: PreviewProvider {
     static var previews: some View {
-        TimeTrackerView()
+        let model = TimeTrackerViewModel(progress: 0.3, currentTimer: "HH:mm:ss")
+        TimeTrackerView(model: model)
     }
 }
